be33514c3cf78fc1bbc77203fcf9bc04103f4b23
arifanf <79965177+arifanf@users.noreply.github.com>
2022-03-25 23:42:32 +0700

    adding partial cars card

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,60 +1,9 @@
-const card = [
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    },
-    {
-        mobil: 'Avanza',
-        harga: 'Rp 700.000',
-        waktu: '2-3 Mei 2022',
-        update: '28 April 2022',
-    }
-]
-
+const card = {
+    mobil: 'Avanza',
+    harga: 'Rp 700.000',
+    waktu: '2-3 Mei 2022',
+    update: '28 April 2022',
+} 
 
 module.exports = {
     index: (req, res) => res.render('cars/index', { card }),
